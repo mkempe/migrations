666b54008e922bd3ace48b835cda12cd6c4f3380
Maik Kempe <dev@nrolla.com>
2013-06-04 15:39:35 +0200

    [➦] Merged changes.

---
+++ db/migrate/20130604133854_plain_sql.rb
@@ -0,0 +1,11 @@
+# encoding: utf-8
+
+class PlainSql < ActiveRecord::Migration
+  # def up
+  #   execute "insert into foo values (123,'something');"
+  # end
+
+  # def down
+  #   execute "delete from foo where field='something';"
+  # end
+end
